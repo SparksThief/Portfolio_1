aed0e7ca7acd2a7e2605114f2951c22ee8dd08a1
emilie1567 <emiliebrandstrup@gmail.com>
2024-09-30 21:34:54 +0200

    Rettet nogle flere kommentarer.

--- src/Course.java
+++ src/Course.java
@@ -1,17 +1,19 @@
 // Skaber Course subclass der extender (nedarver) StudyActivity.
 public class Course extends StudyActivity {
-    private boolean isBasicCourse;
+    private boolean isBasicCourse; // Instance variabel
 
-    //Constructor
+    // Constructor
     public Course(int ECTS, boolean isBasicCourse) {
-        super(ECTS); //Kalder konstruktøren fra StudyActivity klassen
-        this.isBasicCourse = isBasicCourse;
+        super(ECTS); // Kalder constructor fra StudyActivity class for at kunne videregive (ECTS);.
+        this.isBasicCourse = isBasicCourse; // Initialiserer instance variabel.
     }
 
+    // Getter så andre dele af programmet kan hente/læse om det er et basiskursus.
     public boolean isBasicCourse() {
         return isBasicCourse;
     }
 
+    // Setter så andre dele af programmet kan ændre/opdatere variablen.
     public void setBasicCourse(boolean isBasicCourse) {
         this.isBasicCourse = isBasicCourse;
     }
--- src/SubjectModule.java
+++ src/SubjectModule.java
@@ -1,22 +1,25 @@
+// Importerer List, så vi kan implementere de forskellige fagmoduler/courses.
 import java.util.List;
 
-// Tilføjer ny class som indeholder en liste over courses og variablen project.
+// Tilføjer ny class med variablen courses som er en liste af typen Course, samt variablen project af typen Project
 public class SubjectModule {
     private List<Course> courses;
     private Project project;
 
-    public List<Course> getCourses(){
+    // Getter - Giver andre klasser adgang til at læse værdien af variablen courses.
+    public List<Course> getCourses() {
         return courses;
     }
-
+    // Setter - Giver andre klasser adgang til at ændre/opdatere værdien af variablen courses.
     public void setCourses(List<Course> courses) {
         this.courses = courses;
     }
 
+    // Getter så andre dele af programmet kan læse værdien af variablen.
     public Project getProject(){
         return project;
     }
-
+    // Setter så andre dele af programmet kan opdatere variablen.
     public void setProject(Project project) {
         this.project = project;
     }
